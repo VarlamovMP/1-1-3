c7627fe6a6f1ae12d02f211114bfef3c7e4f7ff1
Михаил <koko0104199@gmail.com>
2024-11-01 20:18:21 +0300

    Практическая задача 1.1.3 Работа с базой данных c помощью JDBC.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,14 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.saveUser("Анна", "Варламова", (byte)6);
+        userService.saveUser("Григорий", "Варламов", (byte)14);
+        userService.saveUser("Татьяна", "Варламова", (byte)43);
+        userService.saveUser("Михаил", "Варламов", (byte)42);
     }
 }
